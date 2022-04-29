c46de0659504e131a2f780858d92b790d5737eac
Your Name <your_email@whatever.com>
2022-04-29 17:02:28 +0300

    try1 to work with vue integration

---
+++ project3/resources/js/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <Home></Home>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import Home from "../views/HomeView.vue"
+import Navbar from "../components/Navbar.vue"
+
+export default({
+    components: {
+        Home,
+        Navbar,
+    },
+})
+</script>
---
+++ project3/resources/js/views/TestView.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>fdgdgdfgdgfd</h1>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+export default({
+    
+})
+</script>
